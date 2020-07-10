85db786c63e3481f2f0743d916a86dcb86b37f14
sakshi.jaiswal <sakshi.jaiswal@actolap.com>
2020-07-10 13:06:21 +0530

    First Commit: StoryBoard created

---
+++ Flickr Search/New Group/PhotoCollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  PhotoCollectionViewCell.swift
+//  Flickr Search
+//
+//  Created by Sakshi Jaiswal on 10/07/20.
+//  Copyright © 2020 Sakshi Jaiswal. All rights reserved.
+//
+
+import UIKit
+
+class PhotoCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}

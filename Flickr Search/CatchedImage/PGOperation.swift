b860ce0ae3d32fb461ca8c308d0b2bd7543c7755
sakshi.jaiswal <sakshi.jaiswal@actolap.com>
2020-07-13 14:16:11 +0530

    updated code

--- Flickr Search/CatchedImage/PGOperation.swift
+++ Flickr Search/CatchedImage/PGOperation.swift
@@ -13,7 +13,7 @@ class PGOperation: Operation {
     var downloadHandler: ImageDownloadHandler?
     var imageUrl: URL!
     private var indexPath: IndexPath?
-   
+    
     override var isAsynchronous: Bool {
         get {
             return  true
@@ -59,37 +59,25 @@ class PGOperation: Operation {
         self.indexPath = indexPath
     }
     
-    
-   
-    /*override func start() {
-        if self.isCancelled {
-            finish(true)
-        }
-       main()
-    }*/
-    
     override func main() {
         guard isCancelled == false else {
             finish(true)
             return
         }
         self.executing(true)
-        //Asynchronous logic (eg: n/w calls) with callback
         self.downloadImageFromUrl()
     }
     
     func downloadImageFromUrl() {
         let newSession = URLSession.shared
-       let downloadTask = newSession.downloadTask(with: self.imageUrl) { (location, response, error) in
-        if let locationUrl = location, let data = try? Data(contentsOf: locationUrl){
-             let image = UIImage(data: data)
-            self.downloadHandler?(image,self.imageUrl, self.indexPath,error)
-          }
-          self.finish(true)
-          self.executing(false)
+        let downloadTask = newSession.downloadTask(with: self.imageUrl) { (location, response, error) in
+            if let locationUrl = location, let data = try? Data(contentsOf: locationUrl){
+                let image = UIImage(data: data)
+                self.downloadHandler?(image,self.imageUrl, self.indexPath,error)
+            }
+            self.finish(true)
+            self.executing(false)
         }
         downloadTask.resume()
     }
-    
-    
 }

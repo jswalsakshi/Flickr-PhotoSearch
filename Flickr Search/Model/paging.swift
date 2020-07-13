8259657ea2d73059e6a69a01617ad459bf059c50
sakshi.jaiswal <sakshi.jaiswal@actolap.com>
2020-07-13 20:45:40 +0530

    App launch screen Added

--- Flickr Search/Model/paging.swift
+++ Flickr Search/Model/paging.swift
@@ -8,12 +8,12 @@
 
 import Foundation
 
-public class Paging {
+class Paging {
     
-    public var totalPages: Int?
-    public var numberOfElements: Int32?
-    public var currentSize: Int = 20
-    public var currentPage: Int?
+    var totalPages: Int?
+    var numberOfElements: Int32?
+    var currentSize: Int = 20
+    var currentPage: Int?
     
     init(totalPages: Int, elements: Int32, currentPage: Int) {
         self.totalPages = totalPages
